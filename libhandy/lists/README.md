be54535767728cd0bd5702f2ad4a782391e2f097
Jete O'Keeffe <jete@nuc4.attlocal.net>
2020-05-14 15:39:59 -0400

    adding more examples

---
+++ libhandy/lists/README.md
@@ -0,0 +1,1 @@
+https://gitlab.gnome.org/Teams/Design/os-mockups/raw/master/lists/list-design-patterns.png
--- libhandy/lists/radio-css.py
+++ libhandy/lists/radio-css.py
@@ -3,7 +3,7 @@ import gi
 gi.require_version('Handy', '0.0')
 gi.require_version('Gtk', '3.0')
 
-from gi.repository import Gtk, Handy
+from gi.repository import Gtk, Handy, Gio
 
 
 class MyWindow(Gtk.Window):
@@ -15,11 +15,20 @@ class MyWindow(Gtk.Window):
 
         self.set_title("Favorite Browser")
         self.connect("destroy", Gtk.main_quit)
+        self.set_border_width(10)
         self.set_size_request(350,350)
+
+        mystyle = "#myround { border-radius: 20px; }"
+        provider = Gtk.CssProvider.new()
+        provider.load_from_file(Gio.File.new_for_path("radio.css"))
 
             # Create List Box
             # https://lazka.github.io/pgi-docs/Gtk-3.0/classes/ListBox.html 
         box = Gtk.ListBox()
+            # Important to map css
+        box.set_name("myround")
+        style = box.get_style_context()
+        style.add_provider(provider, Gtk.STYLE_PROVIDER_PRIORITY_USER)
         box.set_selection_mode(Gtk.SelectionMode.NONE)
 
             # use the libhandy function to add separators to listbox rows
