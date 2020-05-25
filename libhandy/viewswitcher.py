b90ab6fb28a986ea44cac9edc5f2d0960442cdd6
jete <jete@jete-laptop.attlocal.net>
2020-05-25 11:30:36 -0400

    making workable demo

--- libhandy/viewswitcher.py
+++ libhandy/viewswitcher.py
@@ -8,22 +8,30 @@ from gi.repository import Gtk, Handy
     # https://lazka.github.io/pgi-docs/#Handy-0.0/functions.html#Handy.init 
 Handy.init()
 
+    # Setup HeaderBar
+header = Handy.HeaderBar.new()
+header.set_show_close_button(True)
+
 window = Gtk.Window(title="ViewSwitcher Example")
 window.set_border_width(10)
 
 vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
 
+stack = Gtk.Stack.new()
+stack.add_titled(Gtk.Label.new("Outstanding"), "outstanding", "Outstanding")
+stack.add_titled(Gtk.Label.new("Completed"), "completed", "Completed")
+
+vbox.pack_start(stack, True, True, 0)
+
     # https://lazka.github.io/pgi-docs/#Handy-0.0/classes/ViewSwitcher.html
 switcher = Handy.ViewSwitcher.new()
-switcher.set_stack()
+switcher.set_stack(stack)
 
-stack = Gtk.Stack.new()
+    # Add centered widget
+header.set_custom_title(switcher)
 
-    # https://lazka.github.io/pgi-docs/#Handy-0.0/classes/ViewSwitcherBar.html
-bar = Handy.ViewSwitcherBar.new()
-bar.set_stack(stack)
-
-vbox.pack_start(bar, True, True, 0)
+    # Add header bar
+window.set_titlebar(header)
 
 window.add(vbox)
 window.connect("destroy", Gtk.main_quit)
